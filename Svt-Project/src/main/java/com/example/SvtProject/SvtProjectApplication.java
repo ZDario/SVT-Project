05fde44b3d2f1cfaa07417e57a2467d02bb633d7
zigad <zigadario@gmai.com>
2022-05-13 13:24:48 +0200

    Initial commit.

---
+++ Svt-Project/src/main/java/com/example/SvtProject/SvtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SvtProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvtProjectApplication.class, args);
+	}
+
+}
---
+++ Svt-Project/src/test/java/com/example/SvtProject/SvtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SvtProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
