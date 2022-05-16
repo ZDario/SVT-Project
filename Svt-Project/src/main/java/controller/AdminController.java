731038045dfa0a36003e516fed62bbf544696385
zigad <zigadario@gmai.com>
2022-05-16 19:29:45 +0200

    Implemented controllers with their functionalities.

---
+++ Svt-Project/src/main/java/controller/AdminController.java
@@ -0,0 +1,113 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dto.AdminDTO;
+import model.Admin;
+import model.UserType;
+import serviceInterface.AdminServiceInterface;
+
+@RestController
+@RequestMapping(value = "api/admin")
+public class AdminController {
+
+	@Autowired
+	AdminServiceInterface adminServiceInterface;
+	
+	@GetMapping
+	public ResponseEntity<List<AdminDTO>> getAdmins(){
+
+		List<Admin> admins = adminServiceInterface.findAll();
+		
+		List<AdminDTO> adminDTO = new ArrayList<AdminDTO>();
+		for(Admin admin: admins) {
+			adminDTO.add(new AdminDTO(admin));
+		}
+		return new ResponseEntity<List<AdminDTO>>(adminDTO, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<AdminDTO> getAdmin(@PathVariable("id") Long id){
+		Admin admin = adminServiceInterface.findOne(id);
+		
+		if(admin == null) {
+			return new ResponseEntity<AdminDTO>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<AdminDTO>(new AdminDTO(admin), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<AdminDTO> addAdmin(@RequestBody AdminDTO adminDTO){
+
+		Admin admin = new Admin();
+		admin.setUserName(adminDTO.getUserNameAdmin());
+		admin.setPassword(adminDTO.getPasswordAdmin());
+		admin.setEmail(adminDTO.getEmailAdmin());
+		admin.setAvatar(adminDTO.getAvatarAdmin());
+		admin.setBanned(adminDTO.isBanned());
+		admin.setRegistrationDate(adminDTO.getRegistrationDateAdmin());
+		admin.setUserType(UserType.ADMIN);
+		
+		admin = adminServiceInterface.save(admin);
+		return new ResponseEntity<AdminDTO>(new AdminDTO(admin), HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/{id}", consumes = "application/json")
+	public ResponseEntity<AdminDTO> updateAdmin(@RequestBody AdminDTO adminDTO, @PathVariable("id") Long id){
+
+		Admin admin = adminServiceInterface.findById(id);
+		
+		if(admin == null) {
+			return new ResponseEntity<AdminDTO>(HttpStatus.BAD_REQUEST);
+		}
+		admin.setUserName(adminDTO.getUserNameAdmin());
+		admin.setPassword(adminDTO.getPasswordAdmin());
+		admin.setEmail(adminDTO.getEmailAdmin());
+		admin.setAvatar(adminDTO.getAvatarAdmin());
+		admin.setBanned(adminDTO.isBanned());
+		admin.setRegistrationDate(adminDTO.getRegistrationDateAdmin());
+
+		admin = adminServiceInterface.save(admin);
+		return new ResponseEntity<AdminDTO>(new AdminDTO(admin), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteAdmin(@PathVariable("id") Long id){
+		Admin admin = adminServiceInterface.findById(id);
+		if(admin != null) {
+			adminServiceInterface.remove(id);
+			
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PostMapping(value = "/{id}/blokiranje")
+	public ResponseEntity<AdminDTO> blok(@PathVariable("id") Long id){
+
+		Admin admin = adminServiceInterface.findById(id);
+		
+		if(admin.isBanned() == false) {
+			admin.setBanned(true);
+		}else {
+			admin.setBanned(false);
+		}
+		
+		admin = adminServiceInterface.save(admin);
+		return new ResponseEntity<AdminDTO>(new AdminDTO(admin), HttpStatus.CREATED);
+	}
+	
+}
---
+++ Svt-Project/src/main/java/controller/CommunityController.java
@@ -0,0 +1,95 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dto.CommunityDTO;
+import model.Community;
+import serviceInterface.CommunityServiceInterface;
+
+@RestController
+@RequestMapping(value = "api/community")
+public class CommunityController {
+	
+	@Autowired
+	CommunityServiceInterface communityServiceInterface;
+	
+	@GetMapping
+	public ResponseEntity<List<CommunityDTO>> getCommunities(){
+
+		List<Community> communities = communityServiceInterface.findAll();
+		
+		List<CommunityDTO> communityDTO = new ArrayList<CommunityDTO>();
+		for(Community community: communities) {
+			communityDTO.add(new CommunityDTO(community));
+		}
+		return new ResponseEntity<List<CommunityDTO>>(communityDTO, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CommunityDTO> getCommunity(@PathVariable("id") Long id){
+		Community community = communityServiceInterface.findOne(id);
+		
+		if(community == null) {
+			return new ResponseEntity<CommunityDTO>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<CommunityDTO>(new CommunityDTO(community), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CommunityDTO> addCommunity(@RequestBody CommunityDTO communityDTO){
+
+		Community community = new Community();
+		community.setName(communityDTO.getName());
+		community.setDescription(communityDTO.getDescription());
+		community.setCreationDate(communityDTO.getCreationDate());
+		community.setRules(communityDTO.getRules());
+		community.setSuspended(communityDTO.isSuspended());
+		community.setSuspendedReason(communityDTO.getSuspendedReason());
+		
+		community = communityServiceInterface.save(community);
+		return new ResponseEntity<CommunityDTO>(new CommunityDTO(community), HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/{id}", consumes = "application/json")
+	public ResponseEntity<CommunityDTO> updateCommunity(@RequestBody CommunityDTO communityDTO, @PathVariable("id") Long id){
+
+		Community community = communityServiceInterface.findById(id);
+		
+		if(community == null) {
+			return new ResponseEntity<CommunityDTO>(HttpStatus.BAD_REQUEST);
+		}
+		community.setName(communityDTO.getName());
+		community.setDescription(communityDTO.getDescription());
+		community.setCreationDate(communityDTO.getCreationDate());
+		community.setRules(communityDTO.getRules());
+		community.setSuspended(communityDTO.isSuspended());
+		community.setSuspendedReason(communityDTO.getSuspendedReason());
+
+		community = communityServiceInterface.save(community);
+		return new ResponseEntity<CommunityDTO>(new CommunityDTO(community), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteCommunity(@PathVariable("id") Long id){
+		Community community = communityServiceInterface.findById(id);
+		if(community != null) {
+			communityServiceInterface.remove(id);
+			
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+	}
+}
